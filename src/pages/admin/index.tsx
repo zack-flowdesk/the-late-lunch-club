009cb2bf645a3dc1c9e86809756bd723c064b750
zack-flowdesk <zack.chung@flowdesk.co>
2024-09-21 13:07:45 +0800

    created admin and home page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,13 @@
+// src/pages/admin/index.tsx
+import React from 'react';
+
+const Admin: React.FC = () => {
+    return (
+        <div>
+            <h1>Admin Page</h1>
+            <p>Welcome to the admin page!</p>
+        </div>
+    );
+};
+
+export default Admin;
