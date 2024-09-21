a0f56e83a33da033c34aa198aa0d514a6a0a4d17
zack-flowdesk <zack.chung@flowdesk.co>
2024-09-21 13:12:29 +0800

    minor reformat

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
 import Admin from './pages/admin';
 
 const App: React.FC = () => {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/admin" element={<Admin/>}/>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
